1b7f0e3d1e7bffbb1342854b9cece6cdd08b96d1
skalinator <chris.skaling@gmail.com>
2022-02-17 12:15:19 -0800

    test

--- bootstrap11dev.ps1
+++ bootstrap11dev.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/skalingclouds/inception/master/bootstrap11dev.ps1' -OutFile .\bootstrap11dev.ps1; powershell.exe .\bootstrap11dev.ps1
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/skalingclouds/inception/master/bootstrap11dev.ps1' -OutFile .\bootstrap11dev.ps1; .\bootstrap11dev.ps1
 Write-Host "Starting Post Deploy..."
