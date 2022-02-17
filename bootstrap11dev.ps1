a6cad4e0d488fcd6003bdc790a5bf5788f42d4f9
skalinator <chris.skaling@gmail.com>
2022-02-17 12:35:01 -0800

    gf

--- bootstrap11dev.ps1
+++ bootstrap11dev.ps1
@@ -1,2 +1,18 @@
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/skalingclouds/inception/master/bootstrap11dev.ps1' -OutFile .\bootstrap11dev.ps1; powershell.exe .\bootstrap11dev.ps1
 Write-Host "Starting Post Deploy..."
+$Logfile = $MyInvocation.MyCommand.Path -replace '\.ps1$', '.log'
+ 
+Start-Transcript -Path $Logfile
+ 
+#Doing some stuff with the Verbose parameter
+ 
+Get-ChildItem -Verbose
+ 
+Get-Service -Verbose
+ 
+Get-Process -Verbose
+ 
+Write-Output 'Writing some text to the log file'
+ 
+Stop-Transcript
+#########fc
