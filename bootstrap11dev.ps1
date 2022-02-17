d9d6356c3dbf4f28c5b3907a8dda6101c62f9ace
skalinator <chris.skaling@gmail.com>
2022-02-17 12:36:58 -0800

    remove powershell

--- bootstrap11dev.ps1
+++ bootstrap11dev.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/skalingclouds/inception/master/bootstrap11dev.ps1' -OutFile .\bootstrap11dev.ps1; powershell.exe .\bootstrap11dev.ps1
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/skalingclouds/inception/master/bootstrap11dev.ps1' -OutFile .\bootstrap11dev.ps1; .\bootstrap11dev.ps1
 Write-Host "Starting Post Deploy..."
 $Logfile = $MyInvocation.MyCommand.Path -replace '\.ps1$', '.log'
  
@@ -15,4 +15,4 @@ Get-Process -Verbose
 Write-Output 'Writing some text to the log file'
  
 Stop-Transcript
-#########fc
+#########fcht
