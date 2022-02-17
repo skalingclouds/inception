b994c8700e3f0be266b3ea75995ce19d895c505b
skalinator <chris.skaling@gmail.com>
2022-02-17 12:02:50 -0800

    typo

--- bootstrap11dev.ps1
+++ bootstrap11dev.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/skalingclouds/inception/master/win11devpostdeploy.ps1 -OutFile .\win11devpostdeploy.ps1; powershell.exe .\win11devpostdeploy.ps1
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/skalingclouds/inception/master/bootstrap11dev.ps1 -OutFile .\bootstrap11dev.ps1; powershell.exe .\bootstrap11dev.ps1
 Write-Host "Starting Post Deploy..."
